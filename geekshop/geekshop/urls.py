27cb0c55af7834ae2982c6b90113f415f6c2200c
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-04-03 10:40:19 +0300

    lesson 3 part 2

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,7 +23,8 @@ from mainapp.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),  # сайт администратора
     path('', index, name='index'),  # главная страница
-    # path('products/', products, name='products'),  # страница с товарами
     path('products/', include('mainapp.urls', namespace='mainapp')),
     # path('test/', test, name='test')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from mainapp.models import Product, ProductCategories
 
 # Register your models here.
+admin.site.register(ProductCategories)
+admin.site.register(Product)
