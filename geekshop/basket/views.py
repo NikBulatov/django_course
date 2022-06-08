9505011ac0d801322cfdc314ac1093d5553f10e7
Nikita Bulatov <nikita_bulatov@icloud.com>
2022-06-08 15:08:19 +0300

    F example

--- geekshop/basket/views.py
+++ geekshop/basket/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.db import connection
+from django.db.models import F
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
@@ -17,8 +19,13 @@ def basket_add(request, id):
 
     if baskets:
         basket = baskets.first()
-        basket.quantity += 1
+        basket.quantity += 1  # изменение на уровне python
+        basket.quantity = F(
+            'quantity') + 1  # на уровне СУБД: quantity - из БД берётся, а не из того, что на сайте выберется
         basket.save()
+
+        update_queries = list(filter(lambda x: 'UPDATE' in x['sql'], connection.queries))
+        print(f'basket add {update_queries}')
     else:
         Basket.objects.create(user=user_select, product=product, quantity=1)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
